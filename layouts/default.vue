98911b48491edf0f910d4fe748a369dda130f448
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-01 15:22:36 +0800

    Display all links in home page instead of in header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,9 @@
-<script setup lang="ts">
-import { routes } from "../routes";
-</script>
-
 <template>
   <header>
     <header>
       <nav>
         <ul>
           <li><NuxtLink to="/">Home</NuxtLink></li>
-          <li v-for="{ routeName, displayName } in routes" :key="routeName">
-            <NuxtLink :to="routeName">{{ displayName }}</NuxtLink>
-          </li>
         </ul>
       </nav>
     </header>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "../routes";
+</script>
 
 <template>
-  <div>Home</div>
+  <ul>
+    <li><NuxtLink to="/">Home</NuxtLink></li>
+    <li v-for="{ routeName, displayName } in routes" :key="routeName">
+      <NuxtLink :to="routeName">{{ displayName }}</NuxtLink>
+    </li>
+  </ul>
 </template>
