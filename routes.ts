7713861aa20a41dd7e6d15a8ce226d192589901d
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-01 15:20:12 +0800

    Rewrite to Nuxt

---
+++ routes.ts
@@ -0,0 +1,11 @@
+export interface Route {
+  routeName: string;
+  displayName: string;
+}
+
+export const routes: Route[] = [
+  {
+    routeName: "animating-auto",
+    displayName: "Animating Auto",
+  },
+];
