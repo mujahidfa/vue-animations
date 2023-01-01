15e2f98d97463a6c9f138175136f92097df27c6d
Mujahid Anuar <17759705+mujahidfa@users.noreply.github.com>
2023-01-01 15:23:14 +0800

    Remove home link from home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@ import { routes } from "../routes";
 
 <template>
   <ul>
-    <li><NuxtLink to="/">Home</NuxtLink></li>
     <li v-for="{ routeName, displayName } in routes" :key="routeName">
       <NuxtLink :to="routeName">{{ displayName }}</NuxtLink>
     </li>
